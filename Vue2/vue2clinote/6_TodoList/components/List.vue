e83b228a1521075d6617f7a09223d26a9eff7520
Kenson <3049425262@qq.com>
2025-10-16 21:57:51 +0800

    TodoList增加动画效果

--- Vue2/vue2clinote/6_TodoList/components/List.vue
+++ Vue2/vue2clinote/6_TodoList/components/List.vue
@@ -1,6 +1,9 @@
 <template>
     <ul class="todo-list">
-        <Item v-for="todo in todos" :key="todo.id" :todo="todo" />
+        <!--给每一个item组件加上动画效果，初次渲染时也会有动画效果 -->
+        <transition-group name="todo" appear>
+            <Item v-for="todo in todos" :key="todo.id" :todo="todo" />
+        </transition-group>
     </ul>
 </template>
 
@@ -23,5 +26,25 @@ export default {
     border: 1px solid #ddd;
     border-radius: 2px;
     padding: 0;
+}
+
+/* 进入过程的动画 */
+.todo-enter-active {
+    animation: todo-animate 0.5s linear;
+}
+
+/* 离开过程的动画 */
+.todo-leave-active {
+    animation: todo-animate 0.5s linear reverse;
+}
+
+/* 动画效果 */
+@keyframes todo-animate {
+    from {
+        transform: translateX(100%);
+    }
+    to {
+        transform: translateX(0);
+    }
 }
 </style>
