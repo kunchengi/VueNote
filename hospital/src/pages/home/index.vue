2538bf94f86e5fe17f4cdb2aeb374d6faf9ca8ab
Kenson <3049425262@qq.com>
2025-11-30 22:37:28 +0800

    首页医院地区组件静态页面搭建

--- hospital/src/pages/home/index.vue
+++ hospital/src/pages/home/index.vue
@@ -9,16 +9,19 @@
         <el-col :span="20">
           <!-- 医院等级组件 -->
           <Level />
+          <!-- 医院区域组件 -->
+          <Region />
         </el-col>
         <el-col :span="4">右侧内容</el-col>
     </el-row>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts" name="Home">
 import Carousel from './carousel/index.vue'
 import Search from './search/index.vue'
 import Level from './level/index.vue'
+import Region from './region/index.vue'
 </script>
 
 <style lang="scss" scoped>
---
+++ hospital/src/pages/home/region/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="region">
+    <div class="content">
+        <div class="left">地区：</div>
+        <ul class="right">
+            <li class="active">全部</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+            <li>昌平区</li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts" name="Region">
+</script>
+
+<style lang="scss" scoped>
+.region{
+    color: #7f7f7f;
+    margin-top: 10px;
+    .content{
+        display: flex;
+        .left{
+            margin-right: 10px;
+            width: 53px;
+        }
+        .right{
+            display: flex;
+            flex-wrap: wrap;
+            li{
+                margin-right: 10px;
+                margin-bottom: 10px;
+                &.active{
+                    color: #55a6fe;
+                }
+                &:hover{
+                    color: #55a6fe;
+                    cursor: pointer;
+                }
+            }
+        }
+    }
+}
+</style>
