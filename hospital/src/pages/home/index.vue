0e54fad5b595f9f3a173ca9d2ee5302880b0ab8e
Kenson <3049425262@qq.com>
2025-11-30 22:26:36 +0800

    首页医院等级组件静态页面搭建

--- hospital/src/pages/home/index.vue
+++ hospital/src/pages/home/index.vue
@@ -4,12 +4,21 @@
     <Carousel />
     <!-- 医院搜索框 -->
     <Search />
+    <!-- 底部展示医院的结构 -->
+     <el-row :gutter="20">
+        <el-col :span="20">
+          <!-- 医院等级组件 -->
+          <Level />
+        </el-col>
+        <el-col :span="4">右侧内容</el-col>
+    </el-row>
   </div>
 </template>
 
 <script setup>
 import Carousel from './carousel/index.vue'
 import Search from './search/index.vue'
+import Level from './level/index.vue'
 </script>
 
 <style lang="scss" scoped>
