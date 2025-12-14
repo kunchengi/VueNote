7c5a81b0ba4936105b6c3221b8b57e1b888dc046
Kenson <3049425262@qq.com>
2025-12-14 23:17:02 +0800

    将阿里字体图标引入到本地并使用

--- hospital/src/components/tip/header/index.vue
+++ hospital/src/components/tip/header/index.vue
@@ -6,12 +6,7 @@
         </div>
         <div class="right">
             <span>全部</span>
-            <svg t="1765373860116" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-                p-id="11094" width="16" height="16">
-                <path
-                    d="M307.6 104.6c-14.2 14.2-14.2 37.2 0 51.4L655 503.4c2.8 2.9 2.8 7.5 0 10.3L307.6 861.2c-14.2 14.2-14.2 37.2 0 51.4 14.2 14.2 37.2 14.2 51.4 0l347.4-347.4c15.6-15.6 23.4-36 23.4-56.5s-7.8-41-23.4-56.5L359 104.6c-14.2-14.2-37.2-14.2-51.4 0z"
-                    p-id="11095"></path>
-            </svg>
+            <i class="iconfont icon-youjiantou"></i>
         </div>
     </div>
 </template>
