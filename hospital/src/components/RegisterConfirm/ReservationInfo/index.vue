c39ece2d8a4a30b4286531e404024cb25c6f16e6
Kenson <3049425262@qq.com>
2026-01-14 20:58:23 +0800

    预约挂号确认页面挂号信息静态搭建

---
+++ hospital/src/components/RegisterConfirm/ReservationInfo/index.vue
@@ -0,0 +1,58 @@
+<template>
+    <el-card class="reservation-info" header="挂号信息">
+        <el-descriptions :column="2" border>
+            <el-descriptions-item v-for="(item, index) in reservationInfos" :key="index">
+                <template #label>
+                    <div class="cell-item">
+                        {{ item.label }}
+                    </div>
+                </template>
+                <span :style="{ color: item.key === 'amount' ? '#f5a824' : '#7f7f7f' }">
+                    {{ item.value }}
+                </span>
+            </el-descriptions-item>
+        </el-descriptions>
+    </el-card>
+</template>
+
+<script setup lang="ts" name="ReservationInfo">
+const reservationInfos = [
+    {
+        label: '就诊日期',
+        key: 'reserveDate',
+        value: '2026-01-13 周二 下午',
+    },
+    {
+        label: '就诊医院',
+        key: 'hosName',
+        value: '北京人民医院',
+    },
+    {
+        label: '就诊科室',
+        key: 'deptName',
+        value: '神经内科',
+    },
+    {
+        label: '医生姓名',
+        key: 'docName',
+        value: '张三',
+    },
+    {
+        label: '医生职称',
+        key: 'docTitle',
+        value: '主任医师',
+    },
+    {
+        label: '挂号费',
+        key: 'amount',
+        value: '36',
+    },
+    {
+        label: '医师专长',
+        key: 'skill',
+        value: '擅长神经内科相关疾病的诊治',
+    }
+]
+</script>
+
+<style lang="scss" scoped></style>
--- hospital/src/pages/HosHospital/HospitalRegister/RegisterConfirm/index.vue
+++ hospital/src/pages/HosHospital/HospitalRegister/RegisterConfirm/index.vue
@@ -3,12 +3,22 @@
     <h2 class="confirm-title">确认挂号信息</h2>
     <!-- 就诊人选择 -->
     <confirm-patient />
+    <!-- 挂号信息 -->
+    <reservation-info />
+    <div class="confirm-btn-container">
+      <el-button type="primary" class="confirm-btn" @click="submit">确认挂号</el-button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts" name="RegisterConfirm">
 import ConfirmPatient from '@/components/RegisterConfirm/ConfirmPatient/index.vue'
+import ReservationInfo from '@/components/RegisterConfirm/ReservationInfo/index.vue'
 
+// 提交挂号
+const submit = () => {
+    console.log('提交挂号')
+}
 </script>
 
 <style lang="scss" scoped>
@@ -17,5 +27,15 @@ import ConfirmPatient from '@/components/RegisterConfirm/ConfirmPatient/index.vu
     font-size: 24px;
     font-weight: bold;
   }
+  .confirm-btn-container {
+    display: flex;
+    justify-content: center;
+    margin: 20px 0;
+    .confirm-btn {
+      width: 200px;
+      padding: 20px;
+    }
+  }
+
 }
 </style>
